0263e56fb2ae0f2d100fd194e9bba9080034235c
Michael Germany <mike@rickgermany.com>
2015-03-14 14:28:16 -0600

    code for radio buttons to be able to mark which answer is correct

--- final-project-metatrivia.js
+++ final-project-metatrivia.js
@@ -10,33 +10,53 @@ if (Meteor.isClient) {
   });
 
   Template.body.events({
-  "submit .submitted-question": function (event) {
-    // This function is called when the question submission form is submitted
+    "submit .submitted-question": function (event) {
+      // This function is called when the question submission form is submitted
 
-    var question = event.target.question.value;
-    var answer1 = event.target.answer1.value;
-    var answer2 = event.target.answer2.value;
-    var answer3 = event.target.answer3.value;
-    var answer4 = event.target.answer4.value;
+      var question = event.target.question.value;
+      var answer1 = event.target.answer1.value;
+      var answer2 = event.target.answer2.value;
+      var answer3 = event.target.answer3.value;
+      var answer4 = event.target.answer4.value;
 
-    Tasks.insert({
-      question: question,
-      answers: [
-        {text    : answer1, isAnswer : false},
-        {text    : answer2, isAnswer : false},
-        {text    : answer3, isAnswer : false},
-        {text    : answer4, isAnswer : false}
-        ],
-      createdAt: new Date() // current time
-    });
+      var isAnswer1 = false;
+      var isAnswer2 = false;
+      var isAnswer3 = false;
+      var isAnswer4 = false;
+      if ($('#isAnswer1').is(':checked')) {
+        isAnswer1 = true;
+      }
 
-    // Clear form
-    $('.submitted-question')[0].reset();
+      if ($('#isAnswer2').is(':checked')) {
+        isAnswer2 = true;
+      }
 
-    // Prevent default form submit
-    return false;
-  }
-});
+      if ($('#isAnswer3').is(':checked')) {
+        isAnswer3 = true;
+      }
+
+      if ($('#isAnswer4').is(':checked')) {
+        isAnswer4 = true;
+      }
+
+      Tasks.insert({
+        question: question,
+        answers: [
+          {text: answer1, isAnswer : isAnswer1},
+          {text: answer2, isAnswer : isAnswer2},
+          {text: answer3, isAnswer : isAnswer3},
+          {text: answer4, isAnswer : isAnswer4}
+          ],
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      $('.submitted-question')[0].reset();
+
+      // Prevent default form submit
+      return false;
+    }
+  });
 }
 
 if (Meteor.isServer) {
